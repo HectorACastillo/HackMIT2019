117089468843b3de45dae7e6369ea4cccb335d53
Hector <hectorc@mit.edu>
2019-09-14 19:14:17 -0400

    cleaned up code

--- parse_label.py
+++ parse_label.py
@@ -2,15 +2,10 @@ import json
 
 def parse_label(text):
     '''
-    INPUT:  text - string containing output from classifier to be parsed
+    INPUT:  text - string in json formatt containing output from classifier to be parsed
     OUTPUT: a label as a python string
     '''
     parsed_text = json.loads(text)
-
-    # format the string for parsing
-    #text = text.split('"classes": [')[1]
-    #text = text[:text.find("]")].replace("\n","")
-    #labels = text.split("},{")
     classes = parsed_text["images"][0]["classifiers"][0]["classes"]
 
     # iterate through labels and determine label with the highest score
@@ -21,20 +16,10 @@ def parse_label(text):
         scores.append(float(c['score'])*(d/(d+1)))
     i = scores.index(max(scores))
     return classes[i]['class']
-    
-
-def reference(dic, key):
-    '''
-    INPUT:  dic - string containing a "dictionary" with keys: label, score, and type hierarchy
-            key - key for desired value
-    OUTPUT: the value associated with key
-    '''
-    i = dic.find('"'+key+'": ')+len(key)+4
-    return dic[key]
 
 def get_depth(dic):
     '''
-    INPUT:  dic - string containing a "dictionary" with keys: label, score, and type hierarchy
+    INPUT:  dic - a json dictionary for a particular label class
     OUTPUT: the number of levels in the type hiearchy 
     '''
     if 'type_hierarcy' in dic:
