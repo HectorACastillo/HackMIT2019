3b61a542208e01e989870c86146c06e2196f6cfc
Hector <hectorc@mit.edu>
2019-09-14 19:09:14 -0400

    changed to json approach

--- parse_label.py
+++ parse_label.py
@@ -1,22 +1,26 @@
+import json
+
 def parse_label(text):
     '''
     INPUT:  text - string containing output from classifier to be parsed
     OUTPUT: a label as a python string
     '''
+    parsed_text = json.loads(text)
 
     # format the string for parsing
-    text = text.split('"classes": [')[1]
-    text = text[:text.find("]")].replace("\n","")
-    labels = text.split("},{")
+    #text = text.split('"classes": [')[1]
+    #text = text[:text.find("]")].replace("\n","")
+    #labels = text.split("},{")
+    classes = parsed_text["images"][0]["classifiers"][0]["classes"]
 
     # iterate through labels and determine label with the highest score
     scores = []
-    for label in labels:
-        d = get_depth(label)
+    for c in classes:
+        d = get_depth(c)
         # reference the score provided by the classifier and apply hierarchy depth filter
-        scores.append(float(reference(label,'score'))*(d/(d+1)))
+        scores.append(float(c['score'])*(d/(d+1)))
     i = scores.index(max(scores))
-    return reference(labels[i],'class')
+    return classes[i]['class']
     
 
 def reference(dic, key):
@@ -25,8 +29,8 @@ def reference(dic, key):
             key - key for desired value
     OUTPUT: the value associated with key
     '''
-    i = dic.find('"'+key+'":')+len(key)+3
-    return dic[i:dic.find(',',i)].replace('"','')
+    i = dic.find('"'+key+'": ')+len(key)+4
+    return dic[key]
 
 def get_depth(dic):
     '''
@@ -34,5 +38,5 @@ def get_depth(dic):
     OUTPUT: the number of levels in the type hiearchy 
     '''
     if 'type_hierarcy' in dic:
-        return reference(dic, 'type_hierarcy').count('/')
+        return dic['type_hierarcy'].count('/')
     return 1
