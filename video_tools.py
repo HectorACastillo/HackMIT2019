a3d355052232d838c1eeb9bed3d6f52b58021fd3
Grady Thomas <gradythomas1@Gradys-MacBook-Pro.local>
2019-09-14 20:43:14 -0400

    attempt 4

--- video_tools.py
+++ video_tools.py
@@ -40,11 +40,11 @@ def draw_translation(output_strings, location, image):
     font = ImageFont.truetype("Arial Black.ttf", 15)  
 
     text_size = font.getsize(longer_word)
-    rect_size = (text_size[0]+20, (text_size[1]+5)*2)
+    rect_size = (text_size[0]+8, (text_size[1]+4)*2)
     rect = Image.new('RGBA', rect_size, "black")
     rect_draw = ImageDraw.Draw(rect)
-    rect_draw.text((10, 0), output_strings[2], font=font, fill="#FFFFFF")
-    rect_draw.text((10, text_size[1]+4), output_strings[0], font=font, fill="#808080")
+    rect_draw.text((5, 0), output_strings[2], font=font, fill="#FFFFFF")
+    rect_draw.text((5, text_size[1]+2), output_strings[0], font=font, fill="#808080")
     image_pillow.paste(rect, (location[0], location[1]-(text_size[1]*2+5)))
    
     # Draw the text  
